351561c3a2450f6924f29abe7022783110048482
Craig Hinkley <boxa72@users.noreply.github.com>
2019-07-20 01:43:56 +1000

    Added boxing.java

---
+++ boxing.java
@@ -0,0 +1,16 @@
+package boxing
+
+import java.util.Scanner
+/* Author: Craig Hinkley
+   Date: Sat July 20 2019
+   Time: 1:25am
+   Topic: Boxing Timer App
+*/
+
+public class boxing {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+
+		System.out.println("Welcome to the Boxing Timer");
+	}
+}
